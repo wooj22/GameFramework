b59142e8895a9a1c206b88aaa17b0be79a782997
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-04-21 18:21:19 +0900

    Feat | Player class

---
+++ GameFramework/GameFramework/Player.cpp
@@ -0,0 +1,153 @@
+#include "Player.h"
+
+void Player::Start() {
+	LoadImages();
+}
+
+void Player::Update() {
+	moveTimer += TimeManager::Get().GetDeltaTime();
+	animationTimer += TimeManager::Get().GetDeltaTime();
+	StateUpdate();
+	Move();
+}
+
+void Player::Render() {
+	Animation();
+}
+
+
+/* -------------------------------------------------------------------------- */
+/// Player animation frames load
+void Player::LoadImages() {
+	wchar_t filePath[256];
+
+	// idle
+	for (int i = 0; i < IDLE_SIZE; ++i) {
+		swprintf_s(filePath, L"../Resource/PlayerAnimation/idle-%d.png", i + 1);
+		RenderManager::Get().LoadImageFile(idleFrames[i], filePath);
+	}
+
+	// walk
+	for (int i = 0; i < WALK_SIZE; ++i) {
+		swprintf_s(filePath, L"../Resource/PlayerAnimation/walk-%d.png", i + 1);
+		RenderManager::Get().LoadImageFile(walkFrames[i], filePath);
+	}
+
+	// attack
+	for (int i = 0; i < ATTACK_SIZE; ++i) {
+		swprintf_s(filePath, L"../Resource/PlayerAnimation/attack-A%d.png", i + 1);
+		RenderManager::Get().LoadImageFile(attackFrames[i], filePath);
+	}
+}
+
+/// Player state update (input)
+void Player::StateUpdate() {
+	// walk
+	if (InputManager::Get().IsKeyDown(VK_LEFT)) {
+		preState = curState;
+		curState = WALK;
+		curMoveState = LEFT;
+	}
+	if(InputManager::Get().IsKeyDown(VK_RIGHT)) {
+		preState = curState;
+		curState = WALK;
+		curMoveState = RIGHT;
+	}
+	if (InputManager::Get().IsKeyDown(VK_UP)) {
+		preState = curState;
+		curState = WALK;
+		curMoveState = UP;
+	}
+	if (InputManager::Get().IsKeyDown(VK_DOWN)) {
+		preState = curState;
+		curState = WALK;
+		curMoveState = DOWN;
+	}
+
+	// attack
+	if (InputManager::Get().IsKeyDown(VK_SPACE)) {
+		preState = curState;
+		curState = WALK;
+	}
+
+	// idle
+	if (!InputManager::Get().IsKeyDown(VK_LEFT) &&
+		!InputManager::Get().IsKeyDown(VK_RIGHT) &&
+		!InputManager::Get().IsKeyDown(VK_UP)&&
+		!InputManager::Get().IsKeyDown(VK_DOWN)&&
+		!InputManager::Get().IsKeyDown(VK_SPACE)) {
+		preState = curState;
+		curState = ATTACK;
+	}
+}
+
+/// Player move
+void Player::Move() {
+	if (moveTimer > moveCycle) {
+		switch (curMoveState)
+		{
+		case Player::LEFT:
+			this->pos.X--;
+			break;
+		case Player::RIGHT:
+			this->pos.X++;
+			break;
+		case Player::UP:
+			this->pos.Y--;
+			break;
+		case Player::DOWN:
+			this->pos.Y++;
+			break;
+		default:
+			break;
+		}
+
+		moveTimer = 0;
+	}
+}
+
+/// Animation frame change
+void Player::Animation() {
+	if (animationTimer > animationCycle) {
+		switch (curState)
+		{
+		case Player::IDLE:
+			if (animationIndex > IDLE_SIZE - 1) animationIndex = 0;
+			if(preState != curState) animationIndex = 0;
+
+			RenderManager::Get().DrawImage(idleFrames[animationIndex], 
+				pos.X - (int)idleFrames[animationIndex]->GetWidth()/2,
+				pos.Y - (int)idleFrames[animationIndex]->GetHeight() / 2);
+
+			break;
+		case Player::WALK:
+			if (animationIndex > WALK_SIZE - 1) animationIndex = 0;
+			if (preState != curState) animationIndex = 0;
+
+			RenderManager::Get().DrawImage(walkFrames[animationIndex],
+				pos.X - (int)walkFrames[animationIndex]->GetWidth() / 2,
+				pos.Y - (int)walkFrames[animationIndex]->GetHeight() / 2);
+
+			break;
+		case Player::ATTACK:
+			if (animationIndex > ATTACK_SIZE - 1) animationIndex = 0;
+			if (preState != curState) animationIndex = 0;
+
+			RenderManager::Get().DrawImage(attackFrames[animationIndex],
+				pos.X - (int)attackFrames[animationIndex]->GetWidth() / 2,
+				pos.Y - (int)attackFrames[animationIndex]->GetHeight() / 2);
+
+			break;
+		default:
+			break;
+		}
+
+		animationIndex++;
+		animationTimer = 0;
+	}
+}
+
+/// AABB Collision
+bool Player::isCollision(COORD pos, int width, int height) {
+	return false;
+}
--- GameFramework/GameFramework/Player.h
+++ GameFramework/GameFramework/Player.h
@@ -1,7 +1,57 @@
 #pragma once
-#include "Player.h"
 #include "../GameEngineLib/Object.h"
+#include "../GameEngineLib/framework.h"
+#include "GameManager.h"
+
+// animations frame size
+const int IDLE_SIZE = 4;
+const int WALK_SIZE = 6;
+const int ATTACK_SIZE = 8;
+
 
 class Player :public Object {
+private:
+	// transform
+	COORD pos = {0,0};
+	int width;
+	int height;
+	int winWidth;
+	int winHeight;
 
+	// stat
+	float moveCycle = 0.2f;
+	float animationCycle = 0.0f;
+
+	// timer
+	float moveTimer = 0.0f;
+	float animationTimer = 0.0f;
+
+	// state
+	enum PlayerState { IDLE, WALK, ATTACK };
+	enum PlayerMoveState { LEFT, RIGHT,	UP,	DOWN };
+
+	PlayerState curState = IDLE;
+	PlayerState preState = IDLE;
+	PlayerMoveState curMoveState = LEFT;
+
+	// animations
+	int animationIndex = 0;
+	Bitmap* idleFrames[IDLE_SIZE];
+	Bitmap* walkFrames[WALK_SIZE];
+	Bitmap* attackFrames[ATTACK_SIZE];
+
+public:
+	Player(COORD inputPos, int win_width, int win_Height) : 
+		pos(inputPos), winWidth(win_width), winHeight(win_Height) {};
+	~Player() override = default;
+
+	void Start();
+	void Update() override;	 // scene manager called
+	void Render() override;	 // scene manager called	
+
+	void LoadImages();
+	void StateUpdate();
+	void Move();
+	bool isCollision(COORD pos, int width, int height);
+	void Animation();
 };
