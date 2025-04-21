7fe7c91831d039cb908dff247c74c31ad5fbfa67
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-04-21 21:27:43 +0900

    Feat | Vector2

---
+++ GameFramework/GameEngineLib/Vector2.h
@@ -0,0 +1,23 @@
+#pragma once
+using namespace std;
+
+class Vector2
+{
+public:
+	float x, y;
+	Vector2(float inputX = 0, float inputY = 0) : x(inputX), y(inputY) {};
+
+	// operator
+	Vector2 operator+ (const Vector2& vector2);
+	Vector2 operator- (const Vector2& vector2);
+	Vector2 operator* (float n);
+
+	// member function
+	void Show() { cout << "x : " << x << ", y : " << y << endl; }
+	void Normalize();
+	float Length();
+	float LengthSquared();
+	float Dot(const Vector2& vector2);
+	float Distance(const Vector2& vector2);
+};
+
